a8bbe21868bc7196c4af5d22e0b6ae1797e556d0
TheKelvinT <100819164+TheKelvinT@users.noreply.github.com>
2023-02-15 12:31:08 +0800

    sendgrid api

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,33 @@
 module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
-      jwtSecret: env('JWT_SECRET'),
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
+    },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+  },
+  email: {
+    config: {
+      provider: "sendgrid", // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "tnchnkit@hotmail.com",
+        defaultReplyTo: "kyzenbina@gmail.com",
       },
     },
-  });
+  },
+});
